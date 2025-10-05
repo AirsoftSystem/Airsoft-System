e3c44eeaa2a8e280afeb7a43dc512efc629591d3
AirsoftSystem <airsoftsystemsiege@gmail.com>
2025-10-05 21:46:10 +0200

    Ajout du build Flutter Web dans public/infos

--- public/infos/flutter_bootstrap.js
+++ public/infos/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "153032966"
+    serviceWorkerVersion: "2089623854"
   }
 });
