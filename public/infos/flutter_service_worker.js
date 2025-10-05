b0b3f21f46f675a335c8a9f10343a759108670fa
AirsoftSystem <airsoftsystemsiege@gmail.com>
2025-10-05 19:53:47 +0200

    Ajout du build Flutter Web dans public/infos

--- public/infos/flutter_service_worker.js
+++ public/infos/flutter_service_worker.js
@@ -54,7 +54,7 @@ const RESOURCES = {"68c67ca77a525019301d2af5.glb": "75cd85834d558a2a5132446df480
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "9cf80a203e0c8bd833a0972afdec1a79",
+"flutter_bootstrap.js": "b6f6ff95a74e32531f799b10f80ea85c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
